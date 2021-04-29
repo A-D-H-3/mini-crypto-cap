0d0210747206db7cdfad85ee146427c69827dc13
Avery Hampton <averydhampton@icloud.com>
2021-04-29 18:32:24 -0500

    adds updated API call

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // let content = document.getElementById("content");
   // content.innerHTML =
   fetch(
-    "https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=5000&convert=USD",
+    "https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
     {
       headers: {
         "X-CMC_PRO_API_KEY": "fcddb729-807f-41a8-ac05-f514a689c2f1",
