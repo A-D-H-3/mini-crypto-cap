3f52f5d2fc0e853541e89f8fece97315c47b441f
Avery Hampton <averydhampton@icloud.com>
2021-05-01 13:25:01 -0500

    adds working searchbar w/ error msg

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 const renderCrypto = (coin) => {
   const cryptoMarket = document.getElementById("cryptoMarket");
-  cryptoMarket.innerHTML = `<p>${coin.market_data.current_price.usd}</p>`;
+  cryptoMarket.innerHTML = `<p>${coin.market_data.current_price.usd}</p>
+  <p>${coin.market_cap_rank}</p>`;
+};
+const renderError = () => {
+  const cryptoMarket = document.getElementById("cryptoMarket");
+  cryptoMarket.innerHTML = `<p><a href="https://www.coingecko.com/en">Please use valid Crypto ID!</a></p>`;
 };
 
 document.addEventListener("DOMContentLoaded", function () {
   // console.log(data);
-  const searchForm = document.getElementById("search-field-container");
+  const searchForm = document.getElementById("searchCrypto");
   const searchField = document.getElementById("search-field");
 
   searchForm.addEventListener("submit", function (event) {
     event.preventDefault();
+    const lower = searchField.value.toLowerCase();
+    fetch(`https://api.coingecko.com/api/v3/coins/${lower}`)
+      .then((response) => response.json())
+      .then((coin) => {
+        // const coinData = coin.data;
+        // const found = coinData.find((element) => element.name === "Bitcoin");
+        console.log(coin.market_data.current_price.usd);
+        renderCrypto(coin);
+      })
+      .catch(() => {
+        renderError();
+        console.log("wrong!");
+      });
+
     console.log(searchField.value);
   });
   // let content = document.getElementById("content");
   // content.innerHTML =
-  fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
-    .then((response) => response.json())
-    .then((coin) => {
-      // const coinData = coin.data;
-      // const found = coinData.find((element) => element.name === "Bitcoin");
-      console.log(coin.market_data.current_price.usd);
-      renderCrypto(coin);
-    });
 });
