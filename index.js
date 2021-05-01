a941593796f65c2c31570ec9eaa3cf47a8ec6529
Avery Hampton <averydhampton@icloud.com>
2021-05-01 10:08:39 -0500

    adds and renders data from new API

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
-document.addEventListener("DOMContentLoaded", function(){
-    console.log(data);
+const renderCrypto = (coin) => {
+  const cryptoMarket = document.getElementById("cryptoMarket");
+  cryptoMarket.innerHTML = `<p>${coin.market_data.current_price.usd}</p>`;
+};
 
+document.addEventListener("DOMContentLoaded", function () {
+  // console.log(data);
+  const searchForm = document.getElementById("search-field-container");
+  const searchField = document.getElementById("search-field");
 
-    const cryptoResults = document.getElementById("data-results");
-    const searchForm = document.getElementById("search-field-container");
-    const searchField = document.getElementById("search-field");
-
-    searchForm.addEventListener("submit", function (event){
-        event.preventDefault();
-        console.log(searchField.value);
-    })
+  searchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(searchField.value);
+  });
   // let content = document.getElementById("content");
   // content.innerHTML =
-  fetch(
-    "https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
-    {
-      headers: {
-        "X-CMC_PRO_API_KEY": "fcddb729-807f-41a8-ac05-f514a689c2f1",
-        // 'Content-Type': 'application/x-www-form-urlencoded',
-      },
-    }
-  )
+  fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((coin) => {
+      // const coinData = coin.data;
+      // const found = coinData.find((element) => element.name === "Bitcoin");
+      console.log(coin.market_data.current_price.usd);
+      renderCrypto(coin);
+    });
 });
